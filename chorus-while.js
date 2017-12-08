e55c78fb48bcd022c194491876fbec965f876848
Spencer Greff <spencer.greff@gmail.com>
2017-12-08 07:52:57 +0000

    packing list exercise. Looping through an array.

---
+++ chorus-while.js
@@ -0,0 +1,15 @@
+var chorus = "Let's dance!";
+for (var i = 0; i < 10; i++) {
+  console.log(chorus);
+}
+/* // while loop - still uses chorus variable
+var repeat = 0;
+while (repeat <10) {
+  if (repeat == 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+*/
+console.log("Until the sun come up!");
